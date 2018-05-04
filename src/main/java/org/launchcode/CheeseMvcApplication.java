51c78c614efcbd738e4469d5eb87067b11b7b988
timcliffe <timothy.h.cliffe@gmail.com>
2018-05-04 15:46:44 -0500

    Population calculations done--all that is left is adding new cities.

--- src/main/java/org/launchcode/CheeseMvcApplication.java
+++ src/main/java/org/launchcode/CheeseMvcApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CheeseDc7Application {
+public class CheeseMvcApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CheeseDc7Application.class, args);
+		SpringApplication.run(CheeseMvcApplication.class, args);
 	}
 }
--- src/test/java/org/launchcode/CheeseMvcApplicationTests.java
+++ src/test/java/org/launchcode/CheeseMvcApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class CheeseDc7ApplicationTests {
+public class CheeseMvcApplicationTests {
 
 	@Test
 	public void contextLoads() {
